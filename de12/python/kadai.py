b041881501d9f4ca8823e92b4565b94130a39d50
jyagaimo1 <r202301986jl@jindai.jp>
2023-10-26 11:18:15 +0900

    20231026

--- de12/python/kadai.py
+++ de12/python/kadai.py
@@ -35,10 +35,39 @@ while(True):
     else:
         print("不正解")
 while(True):
-    f=input("電動機付き客車")
+    f=input("電動機付き客車の略称は？")
     if f =="電車":
         print("正解")
         break
     else:
         print("不正解")
+while(True):
+    g=input("首席宰相の略称は？")
+    if g =="首相":
+        print("正解")
+        break
+    else:
+        print("不正解")
+while(True):
+    h=input("経世済民の略称は？")
+    if h =="経済":
+        print("正解")
+        break
+    else:
+        print("不正解")
+while(True):
+    i=input("特定保健用食品の略称は？")
+    if i =="特保":
+        print("正解")
+        break
+    else:
+        print("不正解")
+while(True):
+    j=input("一定の食事献立の略称は？")
+    if j =="定食":
+        print("正解")
+        break
+    else:
+        print("不正解")
+print("おわり！！")
         
